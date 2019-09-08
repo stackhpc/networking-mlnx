d5750be4e2e19035641bf9a3518c6960c4b63fca
Moshe Levi <moshele@mellanox.com>
2019-09-08 23:28:42 +0300

    remote ethtool and ip link dependencies
    Change-Id: I530e9b4e4d2c6b1d226861131c7cdc2fa8399c2f

--- networking_mlnx/eswitchd/utils/pci_utils.py
+++ networking_mlnx/eswitchd/utils/pci_utils.py
@@ -16,13 +16,10 @@ import glob
 import os
 import re
 
-import ethtool
-from oslo_concurrency import processutils
 from oslo_log import log as logging
 
-from networking_mlnx._i18n import _LE, _LW
+from networking_mlnx._i18n import _LE
 from networking_mlnx.eswitchd.common import constants
-from networking_mlnx.eswitchd.utils import command_utils
 
 LOG = logging.getLogger(__name__)
 
@@ -96,28 +93,6 @@ class pciUtils(object):
             return True
         else:
             return
-
-    def get_interface_type(self, ifc):
-        cmd = ['ip', '-o', 'link', 'show', 'dev', ifc]
-        try:
-            out, err = command_utils.execute(*cmd)
-        except (processutils.ProcessExecutionError, OSError) as e:
-            LOG.warning(_LW("Failed to execute command %(cmd)s due to %(e)s"),
-                    {"cmd": cmd, "e": e})
-            raise
-        if out.find('link/ether') != -1:
-            return 'eth'
-        elif out.find('link/infiniband') != -1:
-            return 'ib'
-        else:
-            return None
-
-    def is_ifc_module(self, ifc):
-        if 'ipoib' in ethtool.get_module(ifc):
-            return True
-
-    def filter_ifcs_module(self, ifcs):
-        return [ifc for ifc in ifcs if self.is_ifc_module(ifc)]
 
     def get_pf_mlx_dev(self, pf):
         dev_path = (
